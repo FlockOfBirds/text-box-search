3baf3baf7ad80dbc80d55362fda02364b559cd4f
Joseph D <j_seremba@live.com>
2017-10-10 12:12:53 +0300

    Add configuration files

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const package = require("./package");
+const widgetName = package.widgetName;
+
+const widgetConfig = {
+    entry: `./src/components/${widgetName}Container.ts`,
+    output: {
+        path: path.resolve(__dirname, "dist/tmp"),
+        filename: `src/com/mendix/widget/custom/textboxsearch/${widgetName}.js`,
+        libraryTarget: "umd"
+    },
+    resolve: {
+        extensions: [ ".ts", ".js" ],
+        alias: {
+            "tests": path.resolve(__dirname, "./tests")
+        }
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, use: "ts-loader" },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader!sass-loader"
+                })
+            },
+            {
+                test: /\.gif$/,
+                use: [ {
+                    loader: "url-loader",
+                    options: { limit: 8192 }
+                } ]
+            }
+        ]
+    },
+    devtool: "source-map",
+    externals: [ "react", "react-dom", /^mxui\/|^mendix\/|^dojo\/|^dijit\// ],
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: "src/**/*.xml" },
+        ], { copyUnmodified: true }),
+        new ExtractTextPlugin({ filename: `./src/com/mendix/widget/custom/textboxsearch/ui/${widgetName}.css` }),
+        new webpack.LoaderOptionsPlugin({ debug: true })
+    ]
+};
+
+const previewConfig = {
+    entry: `./src/${widgetName}.webmodeler.ts`,
+    output: {
+        path: path.resolve(__dirname, "dist/tmp"),
+        filename: `src/${widgetName}.webmodeler.js`,
+        libraryTarget: "commonjs"
+    },
+    resolve: {
+        extensions: [ ".ts" ]
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, use: "ts-loader" },
+            { test: /\.css$/, loader: "raw-loader" },
+            { test: /\.scss$/, use: [
+                { loader: "raw-loader" },
+                { loader: "sass-loader" }
+            ] }
+        ]
+    },
+    devtool: "inline-source-map",
+    externals: [ "react", "react-dom" ],
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true })
+    ]
+};
+
+module.exports = [ widgetConfig, previewConfig ];
